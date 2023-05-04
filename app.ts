ea94c32438beaaa9bd27c49cb1daa6a779212325
NurRick <nmospanov@gmail.com>
2023-05-04 21:40:31 +0600

    final commit

--- app.ts
+++ app.ts
@@ -1,18 +1,79 @@
+import express, {Express, Request, Response} from 'express';
 import { connect } from 'mongoose';
 import { ToDoModel } from './models/ToDoModel';
 
+const app: Express = express();
+
+app.use(express.json());
 
 run().catch(err => console.log(err));
 
 async function run() {
-  // 4. Connect to MongoDB
-  await connect('mongodb://127.0.0.1:27017/todolist');
+  try {
+    // 4. Connect to MongoDB
+    await connect('mongodb://127.0.0.1:27017/todolist');
+  } catch (err) {
+    console.log("db connection failed" + err);
+  };
+};
 
-  const todo = new ToDoModel({
-    text: 'acomplish my projects - todolist',
-    status: 'trash',
-  });
-  await todo.save();
+app.get("/todo", async (req: Request, res: Response) => {
+  try {
+    const todo = await ToDoModel.find();
+    res.status(200).json(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something is wrong in server");
+  }
+});
 
-  console.log(todo.status); 
-}
+app.post("/todo/create", async (req: Request, res: Response) => {
+  try {
+    const todo = new ToDoModel({
+      text: 'buy a car',
+      status: 'done',
+    });
+    await todo.save();
+
+    console.log(todo.status);
+
+
+    res.json(todo);
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+app.put("/todo/update/:id/:newstatus", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const statusUpdate = req.params.newstatus;
+    if ( statusUpdate === "todo" || statusUpdate === "done" || statusUpdate === "trash"){
+      const updateData = await ToDoModel.findByIdAndUpdate(id, {status: statusUpdate}, {new: true});
+      res.status(200).json(updateData);
+    } else {
+      res.status(406).send("Incorrect status");
+    } 
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+app.delete("/todo/delete/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const todo = await ToDoModel.findByIdAndDelete(id);
+    res.status(200).send(" document deleted " );
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something is wrong in server");
+  }
+});
+
+
+
+app.listen(8080, () => {
+  console.log("server is running");
+})
--- dist/app.js
+++ dist/app.js
@@ -8,19 +8,82 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const mongoose_1 = require("mongoose");
 const ToDoModel_1 = require("./models/ToDoModel");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
 run().catch(err => console.log(err));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        // 4. Connect to MongoDB
-        yield (0, mongoose_1.connect)('mongodb://127.0.0.1:27017/todolist');
+        try {
+            // 4. Connect to MongoDB
+            yield (0, mongoose_1.connect)('mongodb://127.0.0.1:27017/todolist');
+        }
+        catch (err) {
+            console.log("db connection failed" + err);
+        }
+        ;
+    });
+}
+;
+app.get("/todo", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const todo = yield ToDoModel_1.ToDoModel.find();
+        res.status(200).json(todo);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Something is wrong in server");
+    }
+}));
+app.post("/todo/create", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
         const todo = new ToDoModel_1.ToDoModel({
-            text: 'acomplish my projects - todolist',
-            status: 'trash',
+            text: 'buy a car',
+            status: 'done',
         });
         yield todo.save();
         console.log(todo.status);
-    });
-}
+        res.json(todo);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send(err.message);
+    }
+}));
+app.put("/todo/update/:id/:newstatus", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const id = req.params.id;
+        const statusUpdate = req.params.newstatus;
+        if (statusUpdate === "todo" || statusUpdate === "done" || statusUpdate === "trash") {
+            const updateData = yield ToDoModel_1.ToDoModel.findByIdAndUpdate(id, { status: statusUpdate }, { new: true });
+            res.status(200).json(updateData);
+        }
+        else {
+            res.status(406).send("Incorrect status");
+        }
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send(err.message);
+    }
+}));
+app.delete("/todo/delete/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const id = req.params.id;
+        const todo = yield ToDoModel_1.ToDoModel.findByIdAndDelete(id);
+        res.status(200).send(" document deleted ");
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Something is wrong in server");
+    }
+}));
+app.listen(8080, () => {
+    console.log("server is running");
+});
